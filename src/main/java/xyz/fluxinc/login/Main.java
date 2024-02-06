3fffed0d5782301d50446fff0d85b0f98955a710
Celeste Gateley <celeste.gateley@gmail.com>
2024-02-06 22:04:14 +0100

    Implemented basic commandline login system

--- src/main/java/xyz/fluxinc/login/Main.java
+++ src/main/java/xyz/fluxinc/login/Main.java
@@ -1,8 +1,34 @@
 package xyz.fluxinc.login;
 
+import xyz.fluxinc.login.exceptions.LoginFailedException;
+import xyz.fluxinc.login.users.HardcodedUserProvider;
+import xyz.fluxinc.login.users.User;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        LoginHandler handler = new LoginHandler(new HardcodedUserProvider());
+        Scanner scanner = new Scanner(System.in);
+        User user = null;
+        int attempts = 3;
+        do {
+            System.out.print("Username: ");
+            String username = scanner.nextLine();
+            System.out.print("Password: ");
+            String password = scanner.nextLine();
+            try {
+                user = handler.login(username, password);
+            } catch (LoginFailedException e) {
+                System.out.println(e.getMessage());
+            }
+            attempts--;
+        } while (user == null && attempts > 0);
+        if (user != null) {
+            System.out.println("Hello, " + user.getUsername() + "!");
+        } else {
+            System.out.println("Max attempts reached. Please try again later!");
+        }
     }
 }
